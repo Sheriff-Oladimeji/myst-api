d4e6b91c4a6ddae1c9de60681c3256c63c70e282
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-01 13:27:23 +0100

    fix: update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ dotenv.config()
 const port = process.env.PORT || 5000
 const app = express()
 
-
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 
 app.listen(port, () => {
