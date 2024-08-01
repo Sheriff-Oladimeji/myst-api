0cdc389fbb4f422ccd2c309468aef26deb207698
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-01 13:25:01 +0100

    feat: add server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const port = process.env.PORT || 5000
+const app = express()
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
