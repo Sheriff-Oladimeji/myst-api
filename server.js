bdb272fa223f241739740ef4bcac19ece4efee2a
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-02 15:16:31 +0100

    feat: add router to server

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const connectToDB = require("./db/db")
-dotenv.config()
+const express = require("express");
+const dotenv = require("dotenv");
+const connectToDB = require("./db/db");
+const router = require("./routes/PostRoute");
 
+dotenv.config();
 
-const port = process.env.PORT || 5000
-const app = express()
+const port = process.env.PORT || 5000;
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-connectToDB()
+app.use("/api/posts", router);
+
+connectToDB();
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
-
+  console.log(`Server is running on port ${port}`);
+});
