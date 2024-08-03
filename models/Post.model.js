e2330c4a93a8e2d969fc543b94180e93968f46c8
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-03 06:47:29 +0100

    fix: modify post model

--- models/Post.model.js
+++ models/Post.model.js
@@ -5,6 +5,13 @@ const PostSchema = new mongoose.Schema(
     quote: { type: String, required: true },
     upVote: { type: Number, default: 0 },
     author: { type: String, required: true },
+    tags: [String],
+    image: String,
+    category: {
+      type: String,
+      enums: ["general", "motivational", "inspirational", "life", "funny"],
+      default: "general",
+    },
   },
   { timestamps: true }
 );
