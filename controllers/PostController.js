aebccf7df5566608fdf65a2685462e59d26b15d2
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-08-10 15:31:18 +0100

    feat: add new endpoints

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -35,10 +35,19 @@ const getSinglePost = async (req, res) => {
 
   } catch (error) {
     console.error(error);
-    res.status(404).send("Couldn't find post");
+    res.status(404).send("unable to find post");
   }
 };
 
+const getAllCategories = async (req, res) => {
+  try {
+ const categories = await Post.distinct("category");
+ res.json(categories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 let quoteOfTheDay = null;
 let lastUpdated = null;
@@ -52,7 +61,7 @@ const getRandomPost = async (req, res) => {
      !lastUpdated ||
      now - lastUpdated > 24 * 60 * 60 * 1000
    ) {
-     const posts = await Post.find();
+     const posts = await Post.find({ category: "motivational" });
      quoteOfTheDay = posts[Math.floor(Math.random() * posts.length)];
      lastUpdated = now;
    }
@@ -64,4 +73,23 @@ const getRandomPost = async (req, res) => {
  }
 }
 
-module.exports = { getAllPost, createPost , getSinglePost, getRandomPost};
+
+
+
+const getPostsByCategory = async (req, res) => {
+  try {
+    const category = req.params.category;
+    const posts = await Post.find({ category });
+    if (posts.length > 0) {
+      res.json(posts);
+    } else {
+      res.status(404).send("No posts found for this category");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+
+module.exports = { getAllPost, createPost , getSinglePost, getRandomPost, getAllCategories, getPostsByCategory};
--- routes/PostRoute.js
+++ routes/PostRoute.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { getAllPost, createPost, getSinglePost, getRandomPost } = require("../controllers/PostController");
+const { getAllPost, createPost, getSinglePost, getRandomPost, getAllCategories, getPostsByCategory } = require("../controllers/PostController");
 const router = express.Router();
 
 router.get("/", getAllPost);
 router.post("/", createPost);
+router.get("/categories", getAllCategories);
 router.get("/qod", getRandomPost )
 router.get("/:id", getSinglePost)
+
+router.get("/categories/:category", getPostsByCategory);
 
 module.exports = router;
